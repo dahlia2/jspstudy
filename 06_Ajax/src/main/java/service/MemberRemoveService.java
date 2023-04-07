0958c9f362907d19f5b41d9ab530272b8c03a4d7
dahlia2 <dahlia2p@gmail.com>
2023-04-07 17:37:25 +0900

    2023-04-07

---
+++ 06_Ajax/src/main/java/service/MemberRemoveService.java
@@ -0,0 +1,18 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.Member;
+import repository.MemberDAO;
+
+public class MemberRemoveService implements IMemberService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int memberNo = Integer.parseInt(request.getParameter("memberNo"));
+		
+		int deleteResult = MemberDAO.getInstance().deleteMember(memberNo);
+	}
+
+}
